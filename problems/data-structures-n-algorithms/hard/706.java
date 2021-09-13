9d4ee7ef432f31054e486ec95a0abeaadfafc136
Subendra Sharma <sharma.subendra.kr@gmail.com>
2021-09-14 01:13:01 +0530

    update

---
+++ problems/data-structures-n-algorithms/hard/706.java
@@ -0,0 +1,39 @@
+// 706. Design HashMap
+// https://leetcode.com/problems/design-hashmap/
+
+// class MyHashMap {
+//
+// 	/**
+// 	 * Initialize your data structure here.
+// 	 */
+// 	public MyHashMap () {
+//
+// 	}
+//
+// 	/**
+// 	 * value will always be non-negative.
+// 	 */
+// 	public void put (int key, int value) {
+//
+// 	}
+//
+// 	/**
+// 	 * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+// 	 */
+// 	public int get (int key) {
+//
+// 	}
+//
+// 	/**
+// 	 * Removes the mapping of the specified value key if this map contains a mapping for the key
+// 	 */
+// 	public void remove (int key) {
+//
+// 	}
+// }
+//
+// class S706 {
+// 	public static void main (String[] args) {
+// 		MyHashMap myHashMap = new MyHashMap();
+// 	}
+// }
