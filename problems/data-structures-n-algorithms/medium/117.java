37de9795923e40f45b5e0a1b6fccd6d59c75b8c6
Subendra Sharma <3344394-sharma-subendra-kr@users.noreply.gitlab.com>
2024-12-19 14:06:00 +0530

    update;

--- problems/data-structures-n-algorithms/medium/117.java
+++ problems/data-structures-n-algorithms/medium/117.java
@@ -7,46 +7,92 @@ import java.util.Stack;
 
 class S117 {
 	public static void main (String[] args) {
+		// Node root = new Node(1);
+		// root.left = new Node(2);
+		// root.right = new Node(3);
+		// root.left.left = new Node(4);
+		// root.left.right = new Node(5);
+		// root.right.right = new Node(7);
+
 		Node root = new Node(1);
 		root.left = new Node(2);
 		root.right = new Node(3);
 		root.left.left = new Node(4);
 		root.left.right = new Node(5);
-		root.right.right = new Node(7);
+		root.right.right = new Node(6);
+		root.left.left.left = new Node(7);
+		root.right.right.right = new Node(8);
+
 		Node res = connect(root);
 	}
 
 	public static Node connect (Node root) {
 		if (root == null) {
-			return root;
+			return null;
 		}
-		Deque<Node> deque1 = new ArrayDeque<>();
-		Deque<Node> deque2 = new ArrayDeque<>();
-
-		Node curr = root;
-		deque1.add(curr);
-		while (!deque1.isEmpty()) {
-			curr = deque1.poll();
-			if (curr.left != null) {
-				deque2.add(curr.left);
-			}
-			if (curr.right != null) {
-				deque2.add(curr.right);
-			}
-			if (deque1.isEmpty() && !deque2.isEmpty()) {
-				Node head = deque2.poll();
-				deque1.add(head);
-				Node itr;
-				while (!deque2.isEmpty()) {
-					itr = deque2.poll();
-					deque1.add(itr);
-					head.next = itr;
-					head = head.next;
-				}
-			}
-		}
+		recursion(root);
 		return root;
 	}
+
+	public static Node findLink (Node node, Node curr, Node left) {
+		if (node == null) {
+			return null;
+		}
+		if (curr != node.left && left != node.left && node.left != null) {
+			return node.left;
+		} else if (curr != node.right && node.right != null) {
+			return node.right;
+		}
+		return findLink(node.next, null, null);
+	}
+
+	public static void recursion (Node node) {
+		if (node == null) {
+			return;
+		}
+
+		if (node.left != null) {
+			node.left.next = findLink(node, node.left, null);
+		}
+		if (node.right != null) {
+			node.right.next = findLink(node, node.right, node.left);
+		}
+
+		recursion(node.right);
+		recursion(node.left);
+	}
+
+	// public static Node connect (Node root) {
+	// 	if (root == null) {
+	// 		return root;
+	// 	}
+	// 	Deque<Node> deque1 = new ArrayDeque<>();
+	// 	Deque<Node> deque2 = new ArrayDeque<>();
+	//
+	// 	Node curr = root;
+	// 	deque1.add(curr);
+	// 	while (!deque1.isEmpty()) {
+	// 		curr = deque1.poll();
+	// 		if (curr.left != null) {
+	// 			deque2.add(curr.left);
+	// 		}
+	// 		if (curr.right != null) {
+	// 			deque2.add(curr.right);
+	// 		}
+	// 		if (deque1.isEmpty() && !deque2.isEmpty()) {
+	// 			Node head = deque2.poll();
+	// 			deque1.add(head);
+	// 			Node itr;
+	// 			while (!deque2.isEmpty()) {
+	// 				itr = deque2.poll();
+	// 				deque1.add(itr);
+	// 				head.next = itr;
+	// 				head = head.next;
+	// 			}
+	// 		}
+	// 	}
+	// 	return root;
+	// }
 
 	static class Node {
 		public int val;
