38194b2de5c4b5b3f61d9a7cc7f87bb57330a58d
Subendra Sharma <3344394-sharma-subendra-kr@users.noreply.gitlab.com>
2025-01-01 17:33:24 +0530

    update;

--- problems/data-structures-n-algorithms/medium/63.java
+++ problems/data-structures-n-algorithms/medium/63.java
@@ -1,6 +1,7 @@
 // 63. Unique Paths II
 // https://leetcode.com/problems/unique-paths-ii/
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 class S63 {
@@ -15,16 +16,20 @@ class S63 {
 	}
 
 	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
-		HashMap<String, Integer> memo = new HashMap<>();
 		int rLen = obstacleGrid.length;
 		int cLen = obstacleGrid[0].length;
+		int[][] memo = new int[rLen][cLen];
+		for (int i = 0; i < rLen; i++) {
+			memo[i] = new int[cLen];
+			Arrays.fill(memo[i], 0);
+		}
+
 		int[] count = new int[]{0};
 		recursion(0, 0, obstacleGrid, rLen, cLen, memo, count);
 		return count[0];
 	}
 
-	public static int recursion(int i, int j, int[][] obstacleGrid, int rLen, int cLen, HashMap<String, Integer> memo, int[] count) {
-		String key = i + "," + j;
+	public static int recursion(int i, int j, int[][] obstacleGrid, int rLen, int cLen, int[][] memo, int[] count) {
 		if (i == rLen || j == cLen) {
 			return -1;
 		} else if (obstacleGrid[i][j] == 1) {
@@ -32,26 +37,26 @@ class S63 {
 		} else if (i == rLen - 1 && j == cLen - 1) {
 			count[0]++;
 			return 1;
-		} else if (memo.containsKey(key)) {
-			int val = memo.get(key);
+		} else if (memo[i][j] != 0) {
+			int val = memo[i][j];
 			if (val != -1) {
 				count[0] += val;
 			}
 			return val;
 		}
-		int val = memo.getOrDefault(key, 0);
+		int val = memo[i][j];
 		int res = recursion(i + 1, j, obstacleGrid, rLen, cLen, memo, count);
 		if (res != -1) {
 			val += res;
-			memo.put(key, val);
+			memo[i][j] = val;
 		}
 
 		int res2 = recursion(i, j + 1, obstacleGrid, rLen, cLen, memo, count);
 		if (res2 != -1) {
 			val += res2;
-			memo.put(key, val);
+			memo[i][j] = val;
 		}
-		
+
 		return val;
 	}
 }
