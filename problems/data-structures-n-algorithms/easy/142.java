805e189117b3fdde980ec3b85261e8189c80acb5
Subendra Sharma <32413952+sharma-subendra-kr@users.noreply.github.com>
2025-03-22 21:44:36 +0530

    update;

--- problems/data-structures-n-algorithms/easy/142.java
+++ problems/data-structures-n-algorithms/easy/142.java
@@ -24,13 +24,16 @@ class S142 {
 			return null;
 		}
 		ListNode slow = head;
-		ListNode fast = head.next;
+		ListNode fast = head;
 
-		while (slow != fast && fast != null) {
+		while (fast != null) {
 			slow = slow.next;
 			fast = fast.next;
 			if (fast != null) {
 				fast = fast.next;
+			}
+			if (slow == fast) {
+				break;
 			}
 		}
 
@@ -38,46 +41,73 @@ class S142 {
 			return null;
 		}
 
-		ListNode cut = slow;
-		ListNode cutHead = slow.next;
-		slow.next = null;
-
-		ListNode list1 = head;
-		ListNode list2 = cutHead;
-		ListNode p1 = list1, p2 = list2;
-		int len1 = 0, len2 = 0;
-
-		while (p1 != null) {
-			len1++;
-			p1 = p1.next;
+		slow = head;
+		while (slow != fast) {
+			slow = slow.next;
+			fast = fast.next;
 		}
-
-		while (p2 != null) {
-			len2++;
-			p2 = p2.next;
-		}
-
-		int diff = Math.abs(len1 - len2);
-
-		ListNode longer = list1;
-		ListNode shorter = list2;
-		if (len1 < len2) {
-			longer = list2;
-			shorter = list1;
-		}
-		while (diff != 0) {
-			diff--;
-			longer = longer.next;
-		}
-
-		while (shorter != longer) {
-			shorter = shorter.next;
-			longer = longer.next;
-		}
-		cut.next = cutHead;
-
-		return shorter;
+		return slow;
 	}
+
+	// public static ListNode detectCycle (ListNode head) {
+	// 	if (head == null) {
+	// 		return null;
+	// 	}
+	// 	ListNode slow = head;
+	// 	ListNode fast = head.next;
+	//
+	// 	while (slow != fast && fast != null) {
+	// 		slow = slow.next;
+	// 		fast = fast.next;
+	// 		if (fast != null) {
+	// 			fast = fast.next;
+	// 		}
+	// 	}
+	//
+	// 	if (fast == null) {
+	// 		return null;
+	// 	}
+	//
+	// 	ListNode cut = slow;
+	// 	ListNode cutHead = slow.next;
+	// 	slow.next = null;
+	//
+	// 	ListNode list1 = head;
+	// 	ListNode list2 = cutHead;
+	// 	ListNode p1 = list1, p2 = list2;
+	// 	int len1 = 0, len2 = 0;
+	//
+	// 	while (p1 != null) {
+	// 		len1++;
+	// 		p1 = p1.next;
+	// 	}
+	//
+	// 	while (p2 != null) {
+	// 		len2++;
+	// 		p2 = p2.next;
+	// 	}
+	//
+	// 	int diff = Math.abs(len1 - len2);
+	//
+	// 	ListNode longer = list1;
+	// 	ListNode shorter = list2;
+	// 	if (len1 < len2) {
+	// 		longer = list2;
+	// 		shorter = list1;
+	// 	}
+	// 	while (diff != 0) {
+	// 		diff--;
+	// 		longer = longer.next;
+	// 	}
+	//
+	// 	while (shorter != longer) {
+	// 		shorter = shorter.next;
+	// 		longer = longer.next;
+	// 	}
+	// 	cut.next = cutHead;
+	//
+	// 	return shorter;
+	// }
 
 	static class ListNode {
 		int val;
