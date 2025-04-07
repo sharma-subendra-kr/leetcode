249d8e17ca15e3e509a5dd57e1e8fb2f6d3869f2
Subendra Sharma <32413952+sharma-subendra-kr@users.noreply.github.com>
2025-04-07 15:53:13 +0530

    update;

--- problems/data-structures-n-algorithms/medium/240.java
+++ problems/data-structures-n-algorithms/medium/240.java
@@ -17,28 +17,46 @@ class S240 {
 	}
 
 	public static boolean searchMatrix (int[][] matrix, int target) {
-		int len = matrix.length;
-		int cols = matrix[0].length;
-		int low = 0;
-		int high = cols - 1;
-		int mid;
+		int rows = matrix.length;
+		int cols = matrix[0].length - 1;
+		int r = 0;
+		int c = cols;
 
-		for (int i = 0; i < len; i++) {
-			low = 0;
-			high = cols - 1;
-			while (low < high) {
-				mid = (low + high) / 2;
-				if (target > matrix[i][mid]) {
-					low = mid + 1;
-				} else {
-					high = mid;
-				}
-			}
-			if (matrix[i][low] == target) {
+		while (r < rows && c >= 0) {
+			if (matrix[r][c] > target) {
+				c--;
+			} else if (matrix[r][c] < target) {
+				r++;
+			} else if (matrix[r][c] == target) {
 				return true;
 			}
 		}
-
 		return false;
 	}
+
+	// public static boolean searchMatrix (int[][] matrix, int target) {
+	// 	int len = matrix.length;
+	// 	int cols = matrix[0].length;
+	// 	int low = 0;
+	// 	int high = cols - 1;
+	// 	int mid;
+	//
+	// 	for (int i = 0; i < len; i++) {
+	// 		low = 0;
+	// 		high = cols - 1;
+	// 		while (low < high) {
+	// 			mid = (low + high) / 2;
+	// 			if (target > matrix[i][mid]) {
+	// 				low = mid + 1;
+	// 			} else {
+	// 				high = mid;
+	// 			}
+	// 		}
+	// 		if (matrix[i][low] == target) {
+	// 			return true;
+	// 		}
+	// 	}
+	//
+	// 	return false;
+	// }
 }
